fe9b77542bc1b7a5b45a2632d431deed845b4fbf
karnmnp <114721298+karnmnp@users.noreply.github.com>
2023-01-11 15:43:07 +0700

    change program to nxn

---
+++ ConsoleOX_test_nxn/Model.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleOX1
+{
+    //keep data winplayer
+    public class WinInfo
+    {
+        public WinType Type { get; set; }
+        public int Number { get; set; }
+    }
+
+    //keep GameResult
+    public class GameResult
+    {
+        public Player Winner { get; set; }
+        public WinInfo WinInfo { get; set; }
+    }
+
+    //set player
+    public enum Player
+    {
+        None, X, O
+    }
+
+    //set wintype
+    public enum WinType
+    {
+        Row, Column, MainDiagonal, AntiDiagonal
+    }
+
+    //Model class
+    public class model
+    {
+        //declare n
+        public int generic_value = 4;
+
+        public Player[,] GameGrid { get; set; }
+
+        public Player CurrentPlayer { get; set; }
+        public int TurnsPassed { get; set; }
+        public bool GameOver { get; private set; }
+        public event Action<int, int> MoveMade;
+        public event Action<GameResult> GameEnded;
+        public event Action GameRestarted;
+
+        //start game
+        public model()
+        {
+            GameGrid = new Player[generic_value, generic_value];
+            CurrentPlayer = Player.X;
+            TurnsPassed = 0;
+            GameOver = false;
+        }
+
+        //check ch don't have XO ?
+        private bool CanMakeMove(int r, int c)
+        {
+            return !GameOver && GameGrid[r, c] == Player.None;
+        }
+
+        //check IsGridFull ?
+        private bool IsGridFull()
+        {
+            return TurnsPassed == (generic_value * generic_value);
+        }
+
+        public void SwitchPlayer()
+        {
+            CurrentPlayer = CurrentPlayer == Player.X ? Player.O : Player.X;
+        }
+
+        //Use for check win
+        private bool AreSquaresMarked((int, int)[] squares, Player player)
+        {
+
+            foreach ((int r, int c) in squares)
+            {
+                if (GameGrid[r, c] != player)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        //checkwin
+        public bool DidMoveWin(int r, int c, out WinInfo winInfo)
+        {
+            // Check win here !!
+            (int, int)[] row = { };
+            for (int i = 0; i < generic_value; i++)
+            {
+                row = row.Append((r, i)).ToArray();
+            }
+            (int, int)[] col = { };
+            for (int i = 0; i < generic_value; i++)
+            {
+                col = col.Append((i, c)).ToArray();
+            }
+            (int, int)[] mainDiag = { };
+            for (int i = 0; i < generic_value; i++)
+            {
+                mainDiag = mainDiag.Append((i, i)).ToArray();
+            }
+            (int, int)[] antiDiag = { };
+            for (int i = 0; i < generic_value; i++)
+            {
+                antiDiag = antiDiag.Append((i, generic_value - i - 1)).ToArray();
+            }
+
+            if (AreSquaresMarked(row, CurrentPlayer))
+            {
+                winInfo = new WinInfo { Type = WinType.Row, Number = r };
+                return true;
+            }
+
+            if (AreSquaresMarked(col, CurrentPlayer))
+            {
+                winInfo = new WinInfo { Type = WinType.Column, Number = c };
+                return true;
+            }
+
+            if (AreSquaresMarked(mainDiag, CurrentPlayer))
+            {
+                winInfo = new WinInfo { Type = WinType.MainDiagonal };
+                return true;
+            }
+
+            if (AreSquaresMarked(antiDiag, CurrentPlayer))
+            {
+                winInfo = new WinInfo { Type = WinType.AntiDiagonal };
+                return true;
+            }
+
+            winInfo = null;
+            return false;
+        }
+
+        //condition foe end game
+        public bool DidMoveEndGame(int r, int c, out GameResult gameResult)
+        {
+            if (DidMoveWin(r, c, out WinInfo winInfo))
+            {
+                gameResult = new GameResult { Winner = CurrentPlayer, WinInfo = winInfo };
+                return true;
+            }
+
+            if (IsGridFull())
+            {
+                gameResult = new GameResult { Winner = Player.None };
+                return true;
+            }
+
+            gameResult = null;
+            return false;
+        }
+
+        //use when player click
+        public void MakeMove(int r, int c)
+        {
+            if (!CanMakeMove(r, c))
+            {
+                return;
+            }
+
+            GameGrid[r, c] = CurrentPlayer;
+            TurnsPassed++;
+
+            if (DidMoveEndGame(r, c, out GameResult gameResult))
+            {
+                GameOver = true;
+                MoveMade?.Invoke(r, c);
+                GameEnded?.Invoke(gameResult);
+            }
+            else
+            {
+                SwitchPlayer();
+                MoveMade?.Invoke(r, c);
+            }
+        }
+
+        private void LoadOnMoveMade(int r, int c, char PlayerMarked, string LoadCurrentPlayer)
+        {
+            if (string.Equals(LoadCurrentPlayer[0], 'x'))
+            {
+                CurrentPlayer = Player.X;
+
+            }
+            else if (string.Equals(LoadCurrentPlayer[0], 'o'))
+            {
+                CurrentPlayer = Player.O;
+            }
+
+            if (string.Equals(PlayerMarked, 'x'))
+            {
+                GameGrid[r, c] = Player.X;
+                Player player = GameGrid[r, c];
+
+            }
+            else if (string.Equals(PlayerMarked, 'o'))
+            {
+                GameGrid[r, c] = Player.O;
+                Player player = GameGrid[r, c];
+
+            }
+        }
+
+        public void Reset()
+        {
+            GameGrid = new Player[generic_value, generic_value];
+            CurrentPlayer = Player.X;
+            TurnsPassed = 0;
+            GameOver = false;
+            GameRestarted?.Invoke();
+        }
+
+        public void SaveGame()
+        {
+            // create file save all data
+            FileStream SaveGame = new FileStream("C:\\TicTacToe\\save.txt", FileMode.Create);
+
+            StreamWriter writer_SaveGame = new StreamWriter(SaveGame);
+
+            int turn_passed = 0;
+
+            //Save nxn grid 
+            writer_SaveGame.WriteLine(generic_value);
+
+            //Save grid that PlayersMarked
+            for (int i = 0; i < (generic_value); i++)
+            {
+                for (int j = 0; j < (generic_value); j++)
+                {
+                    if (GameGrid[i, j] != Player.None)
+                    {
+                        writer_SaveGame.Write(GameGrid[i, j].ToString().ToLower());
+
+                        turn_passed++;
+                    }
+
+                    else
+                    {
+                        writer_SaveGame.Write("n");
+                    }
+                }
+            }
+
+            //Save Current_Players
+            writer_SaveGame.WriteLine(" ");
+            writer_SaveGame.WriteLine(CurrentPlayer.ToString().ToLower());
+
+            //Save Turn_passed
+            writer_SaveGame.WriteLine(turn_passed);
+
+            writer_SaveGame.Close();
+            SaveGame.Close();
+        }
+        public void LoadGame()
+        {
+
+            Reset();
+
+            //Load save all data 
+            StreamReader reader = new StreamReader("C:\\TicTacToe\\Save.txt");
+
+            //Load nxn_array
+            string line;
+            line = reader.ReadLine();
+            int nxn_array = Convert.ToInt32(line);
+
+            //Set generic_value = nxn_array
+            generic_value = nxn_array;
+            Trace.WriteLine("This is " + nxn_array + "X" + nxn_array + " array");
+
+
+            //Load Grid Marked
+            line = reader.ReadLine();
+            int length = line.Length; length--;
+            int pos = 0; int row = 0; int col = 0;
+
+            //Load CurrentPlayer
+            string LoadCurrentPlayer;
+            LoadCurrentPlayer = reader.ReadLine();
+            //view.Board(LoadCurrentPlayer);
+            Trace.WriteLine("CurrentPlayer is " + LoadCurrentPlayer);
+
+            while (pos < length)
+            {
+                if (col == nxn_array)
+                {
+                    row++;
+                    col = 0;
+                }
+
+                char PlayerMarked = line[pos];
+                if (PlayerMarked.ToString() != "n")
+                {
+                    view.arr[pos + 1] = PlayerMarked.ToString().ToUpper();
+                    TurnsPassed++;
+                }
+                else
+                {
+                    view.arr[pos + 1] = (pos + 1).ToString();
+                }
+
+                LoadOnMoveMade(row, col, PlayerMarked, LoadCurrentPlayer);
+
+                pos++;
+                col++;
+
+            }
+
+            //close the file
+            reader.Close();
+
+            view.Board(CurrentPlayer.ToString());
+        }
+    }
+}
---
+++ ConsoleOX_test_nxn/view.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleOX1
+{
+    class view
+    {
+        // arr need to be 1 2 3 4 5 ... nxn
+        public static string[] arr = { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" ,"10","11","12","13","14","15","16"};
+        public static int i,j,k = 0;
+        //get generic value from model class
+        private static int generic_value=4;
+
+        public static void Board(string CurrentPlayer)
+        {
+            Console.Clear();// whenever loop will be again start then screen will be clear
+            Console.WriteLine("Player1:X and Player2:O");
+            Console.WriteLine("\n");
+
+            if (CurrentPlayer == "X")//checking the chance of the player
+            {
+                Console.WriteLine("Player X Chance");
+            }
+            else
+            {
+                Console.WriteLine("Player O Chance");
+            }
+            for (i = 0; i < generic_value; i++)//start print value
+            {
+                for (j = 0; j < generic_value; j++)
+                {
+                    if (i * 4 + j + 1 > 9)
+                    {
+                        Console.Write("  {0}  ", arr[i * 4 + j + 1]);
+                    }
+                    else
+                    {
+                        Console.Write("  {0}   ", arr[i * 4 + j + 1]);
+                    }
+                    
+                }
+                Console.WriteLine(" ");
+            }
+                /*
+                Console.Write("     ");// print top
+                for(k=0;k<generic_value;k++)
+                {
+                    Console.Write("|     ");
+                }
+                Console.WriteLine(" ");
+                for (i = 0; i < generic_value; i++)//start print value
+                {
+                    for (j = 0; j < generic_value; j++)
+                    {
+                        if(i * 4 + j + 1 > 9)
+                        {
+                            Console.Write("  {0} |", arr[i * 4 + j + 1]);
+                        }
+                        else
+                        {
+                            Console.Write("  {0}  |", arr[i*4+j+1]);
+                        }                                        
+                    }
+                    Console.WriteLine(" ");
+                    Console.Write("_____");
+                    for (k = 0; k < generic_value; k++)
+                    {
+                        Console.Write("|_____");
+                    }
+                    Console.WriteLine(" ");
+                }
+                Console.Write("     ");// print bottom
+                for (k = 0; k < generic_value; k++)
+                {
+                    Console.Write("|     ");
+                }
+                Console.WriteLine(" ");
+                */
+                /*Console.WriteLine("     |     |      ");
+                Console.WriteLine("  {0}  |  {1}  |  {2}", arr[1], arr[2], arr[3]);
+                Console.WriteLine("_____|_____|_____ ");
+                Console.WriteLine("     |     |      ");
+                Console.WriteLine("  {0}  |  {1}  |  {2}", arr[4], arr[5], arr[6]);
+                Console.WriteLine("_____|_____|_____ ");
+                Console.WriteLine("     |     |      ");
+                Console.WriteLine("  {0}  |  {1}  |  {2}", arr[7], arr[8], arr[9]);
+                Console.WriteLine("     |     |      ");*/
+            }
+
+        public static void DisplayResult(string winner)
+        {
+            Console.WriteLine("\n");
+
+            if (winner != "None")
+            {
+                Console.WriteLine("WINNER IS " + "PLAYER " + winner);
+            }
+
+            else
+            {
+                Console.WriteLine("DRAW");
+            }
+        }
+    }
+}
