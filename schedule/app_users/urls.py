3a2e599df529dc943ae682ad9605f6b11aca0b79
Anecha Khumjun <s6401012630141@email.kmutnb.ac.th>
2023-03-01 20:43:30 +0700

    edit home and login

--- schedule/app_users/urls.py
+++ schedule/app_users/urls.py
@@ -2,5 +2,6 @@ from django.urls import path , include
 from . import views
 
 urlpatterns = [
-    path('', views.login , name = 'login'),
+    path('home/', views.home , name = 'home'),
+    path('login/', views.login , name = 'login'),
 ]
--- schedule/schedule/urls.py
+++ schedule/schedule/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path , include
 
+from app_users import views 
+
 urlpatterns = [
+
+    path('', views.home ),
     path('admin/', admin.site.urls),
     path('users/', include("app_users.urls")),
 ]
