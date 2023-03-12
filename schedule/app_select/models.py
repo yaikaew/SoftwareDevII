32583b235667f7f2b34e65e8a72c2c3df3c3a7f6
Anecha Khumjun <s6401012630141@email.kmutnb.ac.th>
2023-03-12 21:41:44 +0700

    update

--- schedule/app_select/models.py
+++ schedule/app_select/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Subjects_Test_Date(models.Model):
+    id = models.AutoField(db_column='ID', primary_key=True)  # Field name made lowercase.
+    name = models.CharField(max_length=255,null=True)
+    mid = models.CharField(max_length=255,null=True)
+    final = models.CharField(max_length=255,null=True)
+
