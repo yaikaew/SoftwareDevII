cb64dada4bd16958ba5df1d0977e41af330ae9bb
Anecha Khumjun <s6401012630141@email.kmutnb.ac.th>
2023-03-13 01:57:37 +0700

    update  class_sub

--- schedule/app_schedule/models.py
+++ schedule/app_schedule/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Subjects_info(models.Model):
+    id = models.AutoField(db_column='ID', primary_key=True)  # Field name made lowercase.
+    subjects_name = models.CharField(max_length=255,null=True)
+    section = models.CharField(max_length=255,null=True)
+    day = models.CharField(max_length=255,null=True)
+    time = models.TimeField()
+    professor = models.CharField(max_length=255,null=True)
