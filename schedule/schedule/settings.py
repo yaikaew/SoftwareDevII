eb885dd5da5474afeb56ebe953abda283dfdd44e
Anecha Khumjun <s6401012630141@email.kmutnb.ac.th>
2023-03-12 16:49:55 +0700

    ีupdate select_subject page

--- schedule/schedule/settings.py
+++ schedule/schedule/settings.py
@@ -129,4 +129,5 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = ''
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
