ce110a3df95f17a8c83c3f4d600d71e3ef82c171
Anecha Khumjun <s6401012630141@email.kmutnb.ac.th>
2023-03-15 19:23:59 +0700

    update select btn

--- schedule/app_select/views.py
+++ schedule/app_select/views.py
@@ -2,20 +2,27 @@ from django.shortcuts import render , redirect , get_object_or_404
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from .models import Subjects_Test_Date
-from app_schedule.models import Subjects_info
+from app_schedule.models import Subjects_info , User_subjects
 from django.db.models import Q
 
 # Create your views here.
 @login_required(login_url='login')
 def selects_subject_view(request):
     sub_date = Subjects_Test_Date.objects.all()
+    sub_objects = Subjects_info.objects.all()
     if 'q' in request.GET:
         search = request.GET['q']
         multiple_search = Q(Q(name__icontains=search) | Q(code__icontains=search) | Q(prof__icontains=search))
         sub_name = Subjects_info.objects.filter(multiple_search)
     else:
         sub_name = Subjects_info.objects.none()
-    context = {'sub_date':sub_date, 'sub_name':sub_name}
+    # select_btn
+    if request.method == 'POST':
+        subject_id = request.POST.get('id')
+        user_id = request.user.id
+        # insert name into user table using Django ORM
+        User_subjects.objects.create(user_id_id = user_id , sub_id_id = subject_id)
+    context = {'sub_date':sub_date, 'sub_name':sub_name ,'sub_objects':sub_objects}
     return render(request, 'select_subject.html' , context)
 
 
