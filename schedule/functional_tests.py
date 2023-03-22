d9ca39f00b6cec0c5863627bd1ae86261d220083
SpcyR <rodchomchin7354@gmail.com>
2023-03-22 20:25:06 +0700

    Update functional_tests.py

--- schedule/functional_tests.py
+++ schedule/functional_tests.py
@@ -21,37 +21,46 @@ class NewVisitorTest(unittest.TestCase):
         time.sleep(1)
         #ไปหน้า login
         login = self.browser.find_element(By.XPATH, '//*[@id="section-b2"]/a[1]').click()
+        self.assertEqual(self.browser.current_url, 'http://127.0.0.1:8000/users/login/')
 
         #ใส่username & password 
         input_user = self.browser.find_element(By.XPATH, '/html/body/div/div/form/div[1]/input')
         input_user.send_keys(username)
-        
-        input_psw = self.browser.find_element(By.XPATH, '/html/body/div/div/form/div[2]/input')
-        input_psw.send_keys(password + Keys.ENTER)
+        time.sleep(1)
+        input_password = self.browser.find_element(By.XPATH, '/html/body/div/div/form/div[2]/input')
+        input_password.send_keys(password + Keys.ENTER)
 
         time.sleep(2)
 
         #ไปหน้า select
         select_subjectbtn =  self.browser.find_element(By.XPATH,'/html/body/nav/form/nav/div[2]/a[1]').click()
         time.sleep(2)
+        self.assertEqual(self.browser.current_url, 'http://127.0.0.1:8000/select/')
         
         #เสิร์ชหาวิชาที่จะเลือก ในที่นี้คือ database
         search = self.browser.find_element(By.XPATH, '//*[@id="section-a"]/h1')
         self.browser.execute_script("arguments[0].scrollIntoView(true);",search)
         search_box = self.browser.find_element(By.XPATH, '//*[@id="section-c"]/input')
         time.sleep(2)
-        search_box.send_keys('database' + Keys.ENTER)
+        search_box.send_keys('database') 
+        time.sleep(1)
+        search_box.send_keys(Keys.ENTER)
         time.sleep(2)
 
-        #เจอdatabaseแล้ว เลยลองกดเลือก
+        #เลื่อนไปดูว่าค้นหาเจอใช่ไหม
         searchbtn = self.browser.find_element(By.XPATH, '//*[@id="section-d"]')
         self.browser.execute_script("arguments[0].scrollIntoView(true);",searchbtn)
         time.sleep(2)
-        database = self.browser.find_element(By.XPATH, '//*[@id="section-b"]/div/form/button').send_keys(Keys.ENTER)
+        #ลองกดเลือกวิชา
+        select_database = self.browser.find_element(By.XPATH, '//*[@id="section-b"]/div/form/button').send_keys(Keys.ENTER)
         time.sleep(2)
 
         #ไปหน้า schedule เพื่อดูว่าวิชาที่เลือกขึ้นในตารางแล้ว
         schedule_btn = self.browser.find_element(By.XPATH, '/html/body/nav/form/div/div[2]/a[2]').click()
+        time.sleep(2)
+        self.assertEqual(self.browser.current_url, 'http://127.0.0.1:8000/schedule/')
+        info_exam = self.browser.find_element(By.XPATH, '//*[@id="section-a"]/h1')
+        self.browser.execute_script("arguments[0].scrollIntoView(true);",info_exam)
         time.sleep(2)
 
         #ย้อนกลับไปหน้า select เพื่อลบวิชา database
