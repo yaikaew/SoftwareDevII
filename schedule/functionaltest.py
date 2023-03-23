96fab8dbcdcd2d312e04b0eecfcc13856fb5c026
Napawan Nakasuwan <97944321+ampamp1a@users.noreply.github.com>
2023-03-23 23:12:43 +0700

    addcheck

--- schedule/functionaltest.py
+++ schedule/functionaltest.py
@@ -5,9 +5,10 @@ from selenium.webdriver.common.by import By
 import time
 
 
-username = 'ampamp4a'
-email = 'amp@amp.com'
+username = 'ampamp3a'
+email = 'amp2@amp.com'
 password = '1234'
+web_link = 'http://127.0.0.1:8000/'
 
 class NewVisitorTest(unittest.TestCase):  
 
@@ -20,13 +21,13 @@ class NewVisitorTest(unittest.TestCase):
     def test_regis(self):  
         
         #เป็ดหน้าเว็บ
-        self.browser.get('http://127.0.0.1:8000/') 
+        self.browser.get(web_link) 
         time.sleep(2)
 
         #เลือกปุ่ม Regis
         regis_button =  self.browser.find_element(By.XPATH,'//*[@id="section-b2"]/a[2]')
         regis_button.click()
-        self.assertEqual(self.browser.current_url, 'http://127.0.0.1:8000/users/register/')
+        self.assertEqual(self.browser.current_url, '%s%s'% (web_link,'users/register/'))
 
         #กรอก Username
         input_user = self.browser.find_element(By.XPATH, '/html/body/div/div/form/div[1]/input')
@@ -45,11 +46,12 @@ class NewVisitorTest(unittest.TestCase):
 
         #กรอก Re Password
         input_repassword = self.browser.find_element(By.XPATH, '/html/body/div/div/form/div[4]/input')
-        input_repassword.send_keys(password +Keys.ENTER)
+        input_repassword.send_keys(password + Keys.ENTER)
         time.sleep(2)      
 
-        #มาที่หน้าของ user
-        self.assertEqual(self.browser.current_url, 'http://127.0.0.1:8000/users/2/')
+        #ดูว่ามีชื่อ user อยู่ที่ title หรือไม่
+        title = self.browser.find_element(By.XPATH, '/html/body/nav/form/nav/div[1]/nav-label')
+        self.assertIn(username,title.text)
 
         #กดปุ่มเพื่อไปที่หน้า About us
         Aboutus_btn =  self.browser.find_element(By.XPATH,'/html/body/nav/form/nav/div[2]/a[3]')
